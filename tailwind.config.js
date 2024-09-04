ece5b2678f316f790caca9d170ccc94eec402b26
RNRP21 <renzperalta1317@gmail.com>
2024-09-05 01:12:19 +0800

    git

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,24 @@ module.exports = {
        cp:['Courier Prime', 'monospace']
       },
       screens:{
+      'xsmx': {'max': '465px'},
        'smx': {'max': '640px'},
        'mdx': {'max': '768px'},
-      }
+       'lgx': {'max': '1023px'},
+      },
+      animation: {
+        "loop-scroll": "loop-scroll 12s linear infinite",
+      },
+      keyframes: {
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        }
+      },
+      opacity: {
+        0: '0',
+        100: '1',
+      },
     },
   },
   plugins: [],
